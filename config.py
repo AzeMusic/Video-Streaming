398765c006362a43cb4beebe65fdd16332d56f5d
MR AĞA <99508889+AzeMusic@users.noreply.github.com>
2022-06-24 09:39:00 +0400

    Update config.py

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 from os import getenv
 from dotenv import load_dotenv
 
-if os.path.exists("local.env"):
+if os.path.exists("local.env")
     load_dotenv("local.env")
 
 load_dotenv()
@@ -12,17 +12,17 @@ BOT_TOKEN = getenv("BOT_TOKEN")
 BOT_NAME = getenv("BOT_NAME")
 API_ID = int(getenv("API_ID"))
 API_HASH = getenv("API_HASH")
-OWNER_NAME = getenv("OWNER_NAME", "Dr_Asad_Ali")
-ALIVE_NAME = getenv("ALIVE_NAME", "Asad Ali")
+OWNER_NAME = getenv("OWNER_NAME", "tenha055")
+ALIVE_NAME = getenv("ALIVE_NAME", "Pistoncu AĞA")
 BOT_USERNAME = getenv("BOT_USERNAME")
 ASSISTANT_NAME = getenv("ASSISTANT_NAME")
-GROUP_SUPPORT = getenv("GROUP_SUPPORT", "Shayri_Music_Lovers")
-UPDATES_CHANNEL = getenv("UPDATES_CHANNEL", "AsadSupport")
+GROUP_SUPPORT = getenv("GROUP_SUPPORT", "SOQrup")
+UPDATES_CHANNEL = getenv("UPDATES_CHANNEL", "ledyplaylist")
 SUDO_USERS = list(map(int, getenv("SUDO_USERS").split()))
 COMMAND_PREFIXES = list(getenv("COMMAND_PREFIXES", "/ ! .").split())
 ALIVE_IMG = getenv("ALIVE_IMG", "https://telegra.ph/file/b3ff4cfca14c4e47fdf72.jpg")
-DURATION_LIMIT = int(getenv("DURATION_LIMIT", "400"))
-UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/jankarikiduniya/Dr-Music-Video-Streaming")
+DURATION_LIMIT = int(getenv("DURATION_LIMIT", "10000"))
+UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/AzeMusic/Video-Streaming")
 IMG_1 = getenv("IMG_1", "https://telegra.ph/file/e877179fe18a04192a00a.jpg")
 IMG_2 = getenv("IMG_2", "https://telegra.ph/file/74d45f53fc287e9c316d1.jpg")
 IMG_3 = getenv("IMG_3", "https://telegra.ph/file/54fe4851c51bfd440fcb6.jpg")
@@ -31,8 +31,8 @@ IMG_4 = getenv("IMG_4", "https://telegra.ph/file/059fc2fbad833b5c266f9.jpg")
 # Don't Change It Bro ❣️ 😂
 
 
-MY_SERVER = getenv("MY_SERVER", "Shayri_Music_Lovers")
-REPO_OWNER = getenv("REPO_OWNER", "Dr_Asad_Ali")
-MY_HEART = getenv("MY_HEART", "Give_Me_Heart")
-BOT_UPDATE = getenv("BOT_UPDATE", "AsadSupport")
+MY_SERVER = getenv("MY_SERVER", "SOQrup")
+REPO_OWNER = getenv("REPO_OWNER", "tenha055")
+MY_HEART = getenv("MY_HEART", "ruzgar_alican")
+BOT_UPDATE = getenv("BOT_UPDATE", "ledyplaylist")
 MY_BRO = getenv("MY_BRO", "HarshitSharma361")
